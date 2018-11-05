e2aaa923509e02f47c1fc530466e9f8eaa7df1bd
Arnaud BODEL <contact@osw3.net>
2018-11-05 12:46:46 +0100

    -

---
+++ footer.php
@@ -0,0 +1,51 @@
+    <!-- ------------------------ -->
+        </div>
+    </div><!-- End PAGE CONTENT -->
+
+    <!-- FOOTER -->
+    <footer>
+        <div class="container">
+            <div class="footer-nav">
+                <div class="row">
+                    <div class="col-4">
+                        <h3>About</h3>
+                        <nav>
+                            <ul>
+                                <li>About us</li>
+                                <li>Contact</li>
+                                <li>Legal</li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-4">
+                        <h3>Navigation</h3>
+                        <nav>
+                            <ul>
+                                <li>Home</li>
+                                <li>Our Services</li>
+                                <li>Blog</li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-4">
+                        <h3>Last News</h3>
+                        <ul>
+                            <li>News 1</li>
+                            <li>News 2</li>
+                            <li>News 3</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-legal">
+                &copy; copyright 2018
+            </div>
+        </div>
+    </footer>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
